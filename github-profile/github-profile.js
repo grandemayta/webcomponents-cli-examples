2a1f9bba70537c32de20ad59ac5bda32bf3b4804
Mayta Trigozo, G <g.mayta.trigozo@accenture.com>
2019-07-02 14:52:37 +0200

    add examples wcomponents

---
+++ github-profile/github-profile.js
@@ -0,0 +1,27 @@
+class GithubProfile extends HTMLElement {
+    static get observedAttributes() {
+        return ['nickname']
+    }
+
+    get nickname() {
+        return this.getAttribute('nickname')
+    }
+
+    async connectedCallback() {
+        this.render();
+    }
+
+    async attributeChangedCallback() {
+        this.render();
+    }
+
+   async render() {
+        const data = await ( await fetch(`https://api.github.com/users/${this.nickname}`)).json();
+        this.innerHTML = `
+            <h1>${data.name}</h1>
+            <h2>${data.bio}</h2>
+        `
+    }
+}
+
+customElements.define('github-profile', GithubProfile);
