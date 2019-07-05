1398d8b2f1a58812b9ae04a338130ec9405d4dd0
Mayta Trigozo, G <g.mayta.trigozo@accenture.com>
2019-07-05 15:31:36 +0200

    remove connectedCallback lifecycle

--- github-profile/github-profile.js
+++ github-profile/github-profile.js
@@ -2,25 +2,21 @@ class GithubProfile extends HTMLElement {
     static get observedAttributes() {
         return ['nickname'];
     }
-
     get nickname() {
         return this.getAttribute('nickname');
     }
 
-    async connectedCallback() {
+    attributeChangedCallback() {
         this.render();
     }
 
-    async attributeChangedCallback() {
-        this.render();
-    }
-
-   async render() {
+    async render() {
         const data = await ( await fetch(`https://api.github.com/users/${this.nickname}`) ).json();
         this.innerHTML = `
+            <img src=${data.avatar_url}>
             <h1>${data.name}</h1>
             <h2>${data.bio}</h2>
-        `
+        `;
     }
 }
 
